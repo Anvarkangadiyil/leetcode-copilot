912971519d37da5a6e64dbc218ab613df52066dc
Anvarkangadiyil <125873949+Anvarkangadiyil@users.noreply.github.com>
2025-07-13 13:19:24 +0530

    add gitignore

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,12 @@
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.action === 'open_side_panel') {
-    // Open the side panel for the current tab that sent the message
     chrome.sidePanel.open({ tabId: sender.tab?.id! }); //
   }
 });
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }) //
-  .catch((error) => console.error(error)); //
+
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }) 
+  .catch((error) => console.error(error)); 
 
 
 
